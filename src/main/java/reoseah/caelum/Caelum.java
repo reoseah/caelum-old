ff23bc49edd1fb52f337764311c422a195231335
Bohdan <reoseah9@gmail.com>
2020-06-08 19:13:41 +0200

    thanos snap stuff

--- src/main/java/reoseah/caelum/Caelum.java
+++ src/main/java/reoseah/caelum/Caelum.java
@@ -1,35 +1,9 @@
 package reoseah.caelum;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.dimension.v1.FabricDimensionType;
-import net.minecraft.util.Identifier;
-import net.minecraft.world.biome.source.HorizontalVoronoiBiomeAccessType;
-import net.minecraft.world.dimension.DimensionType;
-import reoseah.caelum.client.CaelumParticles;
-import reoseah.caelum.common.CaelumBiomes;
-import reoseah.caelum.common.CaelumBlocks;
-import reoseah.caelum.common.CaelumEntities;
-import reoseah.caelum.common.CaelumFeatures;
-import reoseah.caelum.common.CaelumItems;
-import reoseah.caelum.common.misc.TillingHelper;
-import reoseah.caelum.common.world.CaelumDimension;
 
 public class Caelum implements ModInitializer {
-	public static final DimensionType DIMENSION_TYPE = FabricDimensionType.builder()
-			.factory(CaelumDimension::new)
-			.defaultPlacer(CaelumDimension::placeEntity)
-			.biomeAccessStrategy(HorizontalVoronoiBiomeAccessType.INSTANCE)
-			.buildAndRegister(new Identifier("caelum:caelum"));
-
 	@Override
 	public void onInitialize() {
-		CaelumBlocks.register();
-		CaelumItems.register();
-		CaelumFeatures.register();
-		CaelumBiomes.register();
-		CaelumEntities.register();
-		CaelumParticles.register();
-
-		TillingHelper.registerTilling();
 	}
 }
--- src/main/java/reoseah/caelum/CaelumClient.java
+++ src/main/java/reoseah/caelum/CaelumClient.java
@@ -1,37 +1,9 @@
 package reoseah.caelum;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
-import net.minecraft.client.render.RenderLayer;
-import reoseah.caelum.client.CaelumParticles;
-import reoseah.caelum.client.particle.GlowingDustParticle;
-import reoseah.caelum.client.render.DemonEntityRenderer;
-import reoseah.caelum.common.CaelumBlocks;
-import reoseah.caelum.common.CaelumEntities;
 
 public class CaelumClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutoutMipped(),
-				CaelumBlocks.SKYROOT_SAPLING,
-				CaelumBlocks.SILVER_SKYROOT_SAPLING,
-				CaelumBlocks.DWARF_SKYROOT_SAPLING,
-				CaelumBlocks.CAELUM_SPROUTS,
-				CaelumBlocks.BLOSSOMING_CAELUM_SPROUTS,
-				CaelumBlocks.SKY_BLUE_FLOWER,
-				CaelumBlocks.BARLEY,
-				CaelumBlocks.MUSKMELON_STEM,
-				CaelumBlocks.ATTACHED_MUSKMELON_STEM,
-				CaelumBlocks.CAELUM_OYSTER_SHROOM,
-				CaelumBlocks.CAELUM_OYSTER_SHROOM_PIN,
-				CaelumBlocks.SKYROOT_DOOR,
-				CaelumBlocks.SKYROOT_TRAPDOOR);
-
-		EntityRendererRegistry.INSTANCE.register(CaelumEntities.DEMON, (manager, context) -> new DemonEntityRenderer(manager));
-
-		ParticleFactoryRegistry.getInstance().register(CaelumParticles.GLOWING_DUST, GlowingDustParticle.Factory::new);
 	}
-
 }
